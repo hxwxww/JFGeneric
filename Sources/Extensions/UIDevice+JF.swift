0fa9b1a71d119d8af35c5de11e21e764ed2fea8e
hxwxww <hxwxww@163.com>
2020-04-17 19:12:49 +0800

    update device orientation to ui orientation

--- Sources/Extensions/UIDevice+JF.swift
+++ Sources/Extensions/UIDevice+JF.swift
@@ -22,19 +22,21 @@ extension UIDevice {
     
     /// 是否是竖屏
     public static var isPortrait: Bool {
-        return current.orientation == .portrait || current.orientation == .portraitUpsideDown
+        let orientation = UIApplication.shared.statusBarOrientation
+        return orientation == .portrait || orientation == .portraitUpsideDown
     }
     
     /// 是否是横屏
     public static var isLandscape: Bool {
-        return current.orientation == .landscapeLeft || current.orientation == .landscapeRight
+        let orientation = UIApplication.shared.statusBarOrientation
+        return orientation == .landscapeLeft || orientation == .landscapeRight
     }
     
     /// 是否是IPhoneX
     public static var isIPhoneX: Bool {
         let screenHieght = UIScreen.main.bounds.height
         let screenWidth = UIScreen.main.bounds.width
-        if screenHieght == 812 || screenHieght == 896 || screenWidth == 812 || screenHieght == 896 {
+        if screenHieght == 812 || screenHieght == 896 || screenWidth == 812 || screenWidth == 896 {
             return true
         }
         return false
